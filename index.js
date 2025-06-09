567e30ef5dd19ace040c142746902cc178a1b9dc
John Russo <89462925+JohnRusso8@users.noreply.github.com>
2025-06-09 13:48:20 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head><title>About</title></head>
+<body>
+  <h1>About This App</h1>
+  <a href="index.html">Back to Home</a>
+  <p>Info about the app goes here.</p>
+</body>
+</html>
